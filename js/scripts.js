637894f59297229bd007ed55171fb4b5625da79a
linjojesan <urielpro78@gmail.com>
2017-04-05 17:35:07 -0700

    sample design layout in bootstrap/css

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+
+
+
+$(function() {
+$("#show").click(function(event){
+  $('#button1').click(function () {
+          jQuery(this).children('#deathwell1').first().toggle("slow");
+      });
+
+
+
+
+//  $event.preventDefault();
+  //  $("").show();
+   });
+});
